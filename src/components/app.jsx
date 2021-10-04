b35ba08026cfd9742d1a60296960bf6acb13c1c1
ginotas <ginotas@gmail.com>
2021-10-04 16:49:06 +0300

    leave to next day

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,28 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Header } from "./Header";
 import { Hero } from "./Hero";
-import axios from "axios";
-import Products from "./Products";
+
 
 
 
 function App() {
 
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        axios.get("https://in3.dev/vinted/api/products/").then(function(response) {
-            setProducts(response.data)
-        })
-    }, []);
-
     return (
         <div>
            <Header/>
            <Hero/>
-           <div>
-               {products.map((product) => (<Products key={product.id} data={product}></Products>))}
-           </div>
+           <AllProducts/>
         </div>
     )
 
